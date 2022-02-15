5927f749754061d7dbaad3d9e07261d6584588dc
EdoTO <edobusti@gmail.com>
2022-02-15 09:37:20 +0000

    update

---
+++ app.js
@@ -0,0 +1,51 @@
+const emailList = [
+    'gabibbo@gmail.com',
+    'pinocchio@yahoo.it',
+    'costanzo@libero.it',
+    'paraffa@virgilio.it',
+    'giannimorandi@trap.it'
+];
+
+const userMail = prompt('Inserisci il tuo indirizzo e-mail')
+
+let find = false;
+
+for( let i = 0; i < emailList.length; i++ ) {
+    if(userMail.toLowerCase() === emailList[i].toLowerCase()) {
+        console.log('email trovata')
+        find = true
+        break;
+    } else {
+        console.log('email non trovata')
+    }
+}
+
+if( find === false ) {
+    alert('Stattene fuori')
+} else {
+    alert('Puoi entrare')
+}
+
+const Button = document.getElementById('buttonplay')
+const Message = document.getElementById("message")
+const playerScore = document.getElementById("playerscore")
+let score = 0;
+
+Button.addEventListener("click", function() {
+
+    const player = Math.floor( Math.random () * 6 + 1);
+    const computer = Math.floor( Math.random() * 6 + 1);
+
+    let message = "Parità"
+
+    console.log( player , computer)
+
+    if ( player < computer ) {
+        message = "Hai perso!!";
+    } else if ( player > computer ) {
+        message = "Hai vinto!!";
+    }
+
+    Message.append(message);
+
+})
